3e48f53c6d63251ac576fcd08fc26a579d8f3a19
atanda0x <atandanafiu21@gmail.com>
2024-05-18 17:17:26 -0700

    dotenv and Makefile

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"os"
+
+	"github.com/atanda0x/e-commerce/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9090"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use()
+}
