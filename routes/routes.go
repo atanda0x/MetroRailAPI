1e6f83ef63bbc62d57a32cd2c2ad848548dcd160
atanda0x <atandanafiu21@gmail.com>
2024-05-18 17:10:09 -0700

    users routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,11 @@
 package routes
+
+import "github.com/gin-gonic/gin"
+
+func UserRoutes(inCommingRoute *gin.Engine) {
+	inCommingRoute.POST("/users/signup")
+	inCommingRoute.POST("/users/login")
+	inCommingRoute.POST("/admin/addProduct")
+	inCommingRoute.GET("/users/productView")
+	inCommingRoute.GET("/users/search")
+}
